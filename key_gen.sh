13755db95cc51b9da29efed60b598ee84c11038c
tomo2357 <34735580+tomo2357@users.noreply.github.com>
2024-02-09 21:07:31 +0900

    鍵ファイルをドメイン名に変更

--- key_gen.sh
+++ key_gen.sh
@@ -6,12 +6,12 @@ mkdir -p ./docker/apache_docker/keys
 # 秘密鍵を生成
 openssl genrsa -out ./docker/apache_docker/keys/my_key.key 2048
 
-# CSR（証明書署名要求）を生成
-openssl req -out ./docker/apache_docker/keys/my_cert.csr -key ./docker/apache_docker/keys/my_key.key -new -subj "/C=JP/ST=Tokyo/L=Minato-ku/O=YourOrganization/OU=YourDepartment/CN=localhost"
+# CSR（証明書署名要求）を生成、ドメイン名をwww.chatrobo.comに変更
+openssl req -out ./docker/apache_docker/keys/my_cert.csr -key ./docker/apache_docker/keys/my_key.key -new -subj "/C=JP/ST=Tokyo/L=Minato-ku/O=YourOrganization/OU=YourDepartment/CN=www.chatrobo.com"
 
-# 自己署名証明書を生成
-# オプショナル：SAN（Subject Alternative Name）を含める場合、san.txtファイルのパスを指定
-openssl x509 -req -days 365 -signkey ./docker/apache_docker/keys/my_key.key -in ./docker/apache_docker/keys/my_cert.csr -out ./docker/apache_docker/keys/my_cert.crt -extfile ./docker/apache_docker/apache_san.txt
+# 自己署名証明書を生成、SANを含める
+# 注意：SANを含むための設定ファイル（ここではapache_san.txt）を用意し、適切な設定が含まれていることを確認する
+openssl x509 -req -days 365 -signkey ./docker/apache_docker/keys/my_key.key -in ./docker/apache_docker/keys/my_cert.csr -out ./docker/apache_docker/keys/my_cert.crt -extfile ./docker/apache_docker/apache_san.txt -extensions v3_req
 
 # CSRファイルを削除
 rm ./docker/apache_docker/keys/my_cert.csr
