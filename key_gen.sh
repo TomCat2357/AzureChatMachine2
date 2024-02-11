82fa55e0acacaa13f8d443b808b235e4800e967e
tomo2357 <34735580+tomo2357@users.noreply.github.com>
2024-02-11 23:43:04 +0900

    証明書のURLを直した

--- key_gen.sh
+++ key_gen.sh
@@ -7,7 +7,7 @@ mkdir -p ./docker/apache_docker/keys
 openssl genrsa -out ./docker/apache_docker/keys/my_key.key 2048
 
 # CSR（証明書署名要求）を生成、ドメイン名をwww.chatrobo.comに変更
-openssl req -out ./docker/apache_docker/keys/my_cert.csr -key ./docker/apache_docker/keys/my_key.key -new -subj "/C=JP/ST=Tokyo/L=Minato-ku/O=YourOrganization/OU=YourDepartment/CN=www.chatrobo.com"
+openssl req -out ./docker/apache_docker/keys/my_cert.csr -key ./docker/apache_docker/keys/my_key.key -new -subj "/C=JP/ST=Tokyo/L=Minato-ku/O=YourOrganization/OU=YourDepartment/CN=www.chatrobo.site"
 
 # 自己署名証明書を生成、SANを含める
 # 注意：SANを含むための設定ファイル（ここではapache_san.txt）を用意し、適切な設定が含まれていることを確認する
