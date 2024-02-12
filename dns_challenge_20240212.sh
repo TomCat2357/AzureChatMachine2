76ab6f19b9ecaf0e683800ea6abd94f1b47b40a0
tomo2357 <34735580+tomo2357@users.noreply.github.com>
2024-02-12 20:33:37 +0900

    dnsチャレンジの改良とhostのapache2サーバーの停止

--- dns_challenge_20240212.sh
+++ dns_challenge_20240212.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# システムのパッケージリストを更新
-sudo apt update
-# dnsチャレンジをするためのcerbotをinstall
-sudo DEBIAN_FRONTEND=noninteractive apt install -y \
-    certbot python3-certbot-apache
 
 
 # .envファイルのパス
@@ -27,6 +22,8 @@ if [ -z "$DOMAIN_NAME" ] && [ -z "$EMAIL" ]; then
     exit 1
 fi
 
+
+
 # .pemファイルのパス
 PRIVKEY_PATH="/etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem"
 FULLCHAIN_PATH="/etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem"
@@ -35,8 +32,22 @@ FULLCHAIN_PATH="/etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem"
 
 # .pemファイルが存在しない場合、Certbotコマンドを実行
 if [ ! -f "$PRIVKEY_PATH" ] && [ ! -f "$FULLCHAIN_PATH" ]; then
-    echo "DNSチャレンジを開始します: $DOMAIN_NAME"
+    echo "DNS01チャレンジを開始します: $DOMAIN_NAME"
+    # システムのパッケージリストを更新
+    sudo apt update --quiet
+    # dnsチャレンジをするためのcerbotをinstall
+    sudo DEBIAN_FRONTEND=noninteractive apt install -y --quiet \
+        certbot python3-certbot-apache
+    # apache2が止まっている場合に備えstart
+    sudo service apache2 start
+    # 再起動時にapache2を止めること
+    sudo systemctl disable apache2
+    # apache2を止めることの呼びかけ
+    echo "Please stop apache2.service(sudo service apache2 stop)"
+    #dnsチャレンジ
     sudo certbot certonly --manual --preferred-challenges dns -d $DOMAIN_NAME --email $EMAIL --agree-tos --no-eff-email
+    #apache2を止める
+    sudo service apache2 stop
 
 else
     echo ".pemファイルは既に存在します。"
