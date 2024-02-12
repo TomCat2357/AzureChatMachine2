8aa824d3c1c77263d53b1eb805b5158a255219b9
tomo2357 <34735580+tomo2357@users.noreply.github.com>
2024-02-12 17:26:14 +0900

    Update dns_challenge_20240212.sh

--- dns_challenge_20240212.sh
+++ dns_challenge_20240212.sh
@@ -15,10 +15,15 @@ ENV_FILE="./.env"
 if [ -f "$ENV_FILE" ]; then
     export $(grep -E '^DOMAIN_NAME=' "$ENV_FILE" | xargs)
 fi
+# .envファイルからEMAILを読み込む
+if [ -f "$ENV_FILE" ]; then
+    export $(grep -E '^EMAIL=' "$ENV_FILE" | xargs)
+fi
+
 
 # DOMAIN_NAMEが正しくセットされているか確認
-if [ -z "$DOMAIN_NAME" ]; then
-    echo "DOMAIN_NAMEが.envファイルから読み込めませんでした。"
+if [ -z "$DOMAIN_NAME" ] && [ -z "$EMAIL" ]; then
+    echo "DOMAIN_NAME又はEMAILが.envファイルから読み込めませんでした。"
     exit 1
 fi
 
@@ -31,7 +36,8 @@ FULLCHAIN_PATH="./data/apache_letsencrypt/live/${DOMAIN_NAME}/fullchain.pem"
 # .pemファイルが存在しない場合、Certbotコマンドを実行
 if [ ! -f "$PRIVKEY_PATH" ] && [ ! -f "$FULLCHAIN_PATH" ]; then
     echo "DNSチャレンジを開始します: $DOMAIN_NAME"
-    sudo certbot certonly --manual --preferred-challenges dns -d $DOMAIN_NAME
+    sudo certbot certonly --manual --preferred-challenges dns -d $DOMAIN_NAME --email $EMAIL --agree-tos --no-eff-email
+
 else
     echo ".pemファイルは既に存在します。"
 fi
